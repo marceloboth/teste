7851b1e36994a45b5433cf22ca16f4a9369c17c1
Marcelo J. Both <marcelo.both@gmail.com>
2014-05-26 00:22:09 +0000

    Validação dos campos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  belongs_to :cidade
+
+  validates_presence_of :name, :age
+  validates_uniqueness_of :name
+  validates_length_of :name, minimum: 5, maximum: 50, allow_blank: false
+  validates_numericality_of :age, greater_than: 18
 end
