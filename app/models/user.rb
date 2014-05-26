7aaadae8480feeb7be51c533b63e5b0d15124f1d
Marcelo J. Both <marcelo.both@gmail.com>
2014-05-26 00:29:38 +0000

    Estutura de mensagem de  ero unificada

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :age
   validates_uniqueness_of :name
   validates_length_of :name, minimum: 5, maximum: 50, allow_blank: false
-  validates_numericality_of :age, greater_than: 18
+  validates_numericality_of :age, greater_than: 17
 end
