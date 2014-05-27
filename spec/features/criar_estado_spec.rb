2d10ceebc9ad52f683554980c7cf0ce4ccc0afca
Marcelo J. Both <marcelo.both@gmail.com>
2014-05-27 00:59:44 +0000

    Cadastro de estados

---
+++ spec/features/criar_estado_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Criando estados' do
+  before do
+    visit '/'
+    click_link 'Estados'
+    click_link 'Novo cadastro'
+  end
+
+  scenario 'criar um cadastro' do
+
+  end
+end
